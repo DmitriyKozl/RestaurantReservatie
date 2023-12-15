5053c947906e49e57a4c874b0fce07a0f4ecdd42
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2023-12-15 08:46:58 +0100

    restaurant reservatie start 15/12

--- RestaurantReservatie.BL/Interfaces/ICustomerRepository.cs
+++ RestaurantReservatie.BL/Interfaces/ICustomerRepository.cs
@@ -6,5 +6,4 @@ public interface ICustomerRepository {
     void UpdateCustomer(Customer customer);
     void DeleteCustomer(int id);
     List<Customer> GetCustomers(string filter);
-    void AddCustomer(Customer customer);
-}
+    void AddCustomer(Customer customer); }
